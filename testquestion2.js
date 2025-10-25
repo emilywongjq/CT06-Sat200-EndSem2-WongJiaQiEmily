c76a4a0c03daea8032b468e465dea6c7a4037841
Emily Wong <emily.wongjq@gmail.com>
2025-10-25 15:09:13 +0800

    Update testquestion2.js

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,18 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
+
+function setup(){
+    createCanvas(200, 300);
+    background('black');
+}
+function draw(){
+    fill("pink");
+    textAlign(CENTER,CENTER);
+    textSize(20);
+    yPos = 100;
+    for(let i = 0 ; i < pokemons.length ; i++){
+        text(pokemons[i],100 , 80 + i*30);
+    }
+}
+
 
