5b89fcdfe25d64c6fd3b1b30bef29fbf945cee73
Emily Wong <emily.wongjq@gmail.com>
2025-10-25 15:08:54 +0800

    Update testquestion1.js

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+let posX , posY , xSpeed;
+function setup(){
+    createCanvas(600,400);
+    posX = width/ 2;
+    posY = height/2;
+    xSpeed = 5;
+    fill('limegreen');
+}
+
+function draw(){
+    background('lightblue')
+    posX += xSpeed;
+    circle(posX,posY,50);
+    if(posX - 50/2 <= 0 || posX + 50/2 >=width){
+        xSpeed = xSpeed * -1;
+    }
+
+}
+
+
 
